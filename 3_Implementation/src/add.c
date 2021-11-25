08efb6e7d12bee0b2724460060451461bbb3ffd7
somasundaram2000 <94282752+somasundaram2000@users.noreply.github.com>
2021-11-25 10:49:38 +0530

    Update add.c

--- 3_Implementation/src/add.c
+++ 3_Implementation/src/add.c
@@ -1,1 +1,25 @@
+#include "function.h"
+int add_contact()
+{
+    system("cls");
+        char r[10];
+        char number[10];
+        gets(r);
+        fp=fopen("contact.txt","a+");
+        {
+            printf("\n\n\n\t\t\t\tName : ");
+            scanf("%s",c.name);
+            printf("\n\t\t\t\tPhone Number : ");
+            scanf("%s",&c.phone);
+            printf("\n\t\t\t\tAddress : ");
+            scanf("%s",c.address);
+            printf("\n\t\t\t\tEmail : ");
+            scanf("%s",c.email);
+            fprintf(fp,"%s %s %s %s\n",c.name,c.phone,c.address,c.email);
+            printf("\n\n\t\t\t\tRecord insert successful");
 
+        }
+
+       fclose(fp);
+
+}
